86ce19479bfe46ea0b313bf3d56eebff59a6d493
CNPaul <chinedungene3@gmail.com>
2022-08-23 11:25:58 -0700

    Python if/else statements and loops

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Chinedu Ngene Paul
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Chinedu Ngene Paul
+
+def pow(a, b):
+    return (a ** b)
